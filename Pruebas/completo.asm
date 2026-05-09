bf28ddbf3fc704669c0831990d276d811dcd34a6
SnsRizos <926029@unizar.es>
2026-05-09 13:58:25 +0200

    Arreglo tests

--- Pruebas/completo.asm
+++ Pruebas/completo.asm
@@ -12,42 +12,36 @@ LW R1, 48(R0)   -- Read Miss Clean. Carga Dir 48 en Conjunto 3, Vía 0.
 SW R1, 52(R0)   -- Write Hit. Marca Conjunto 3, Vía 0 Dirty.
 
 
-
 SW R1, 64(R0)   -- Write Miss. Escribe en MP.
 
 
-
-LW R2, 64(R0)   -- Read Miss Clean. Carga Dir 64 en Conjunto 0, Vía 1. (@ X"10000000")
-SW R1, 68(R0)   -- Write Hit. Marca Conjunto 0, Vía 1 Dirty.
-
+LW R4, 64(R0)   -- Read Miss Clean. Carga Dir 64 en Conjunto 0, Vía 1. (@ X"10000000")
 LW R3, 80(R0)   -- Read Miss Clean. Carga Dir 80 en Conjunto 1, Vía 1. (X"00000055")
-SW R1, 84(R0)   -- Write Hit. Marca Conjunto 1, Vía 1 Dirty.
-
-
 LW R1, 96(R0)   -- Read Miss Clean. Carga Dir 96 en Conjunto 2, Vía 1.
-SW R1, 100(R0)  -- Write Hit. Marca Conjunto 2, Vía 1 Dirty.
-
 LW R1, 112(R0)  -- Read Miss Clean. Carga Dir 112 en Conjunto 3, Vía 1.
-SW R1, 116(R0)  -- Write Hit. Marca Conjunto 3, Vía 1 Dirty.
 
 
+SW R3, 0(R4)     -- Escribe X"55" en la palabra 0 de la Scratch (@ X"10000000") Este dato sera modificado si o si porque IO_Master escribe ZZZs en la primera direccion de la Scratch
+SW R3, 4(R4)     -- Escribe X"55" en la palabra 1 de la Scratch (@ X"10000004")
 
-SW R3, 0(R2)     -- Escribe X"55" en la palabra 0 de la Scratch (@ X"10000000") Este dato sera modificado si o si porque IO_Master escribe ZZZs en la primera direccion de la Scratch
-SW R3, 4(R2)     -- Escribe X"55" en la palabra 1 de la Scratch (@ X"10000004")
 
-
-LW R1, 0(R2)     -- Lee de Scratch. R1 -> Seguramente leerá ZZZs
-LW R1, 4(R2)     -- Lee de Scratch. R1 -> X"000000AA"
+LW R1, 0(R4)     -- Lee de Scratch. R1 -> Seguramente leerá ZZZs
+LW R1, 4(R4)     -- Lee de Scratch. R1 -> X"000000AA"
 
 
 LW R3, 128(R0)  -- Read Miss Dirty (Set 0). Expulsa Vía 0 (Dir 0). Copy-Back R3 = X"01000000" 
-LW R2, 144(R0)  -- Read Miss Dirty (Set 1). Expulsa Vía 0 (Dir 16). Copy-Back R2 = X"00000012"
+LW R4, 144(R0)  -- Read Miss Dirty (Set 1). Expulsa Vía 0 (Dir 16). Copy-Back
 LW R1, 160(R0)  -- Read Miss Dirty (Set 2). Expulsa Vía 0 (Dir 32). Copy-Back
 LW R1, 176(R0)  -- Read Miss Dirty (Set 3). Expulsa Vía 0 (Dir 48). Copy-Back
 
 
-SW R2, 0(R3)    -- Intento de escritura en registro interno. Activa Mem_ERROR
-LW R1, 0(R3)    -- Acierto de lectura de registro interno. Desactiva Mem_ERROR
+SW R4, 0(R3)    -- Intento de escritura en registro interno. Activa Mem_ERROR
+
+
+LW R1, 180(R0)  -- Read Miss Clean (Set 0). Expulsa Vía 0 (Dir 64).
+LW R1, 184(R0)  -- Read Miss Clean (Set 1). Expulsa Vía 0 (Dir 80).
+LW R1, 188(R0)  -- Read Miss Clean (Set 2). Expulsa Vía 0 (Dir 96).
+LW R1, 192(R0)  -- Read Miss Clean (Set 3). Expulsa Vía 0 (Dir 112).
 
 
 LW R1, 1(R0)     -- Unaligned
--- Pruebas/falloLecAciertoLecs.asm
+++ Pruebas/falloLecAciertoLecs.asm
@@ -1,70 +1,58 @@
--- Asumimos que R0 = 0.
--- Vamos a usar únicamente R1 para toda la traza.
-
--- ==================================================================
--- FASE 1: Llenado de la VÍA 0 en TODOS LOS CONJUNTOS (Sets 0 al 3)
--- ==================================================================
+-- Vía 0
 -- Set 0
-LW R1, 0(R0)    -- Fallo (Carga Vía 0).  GTKWave R1 -> X"00000001"
-LW R1, 4(R0)    -- Acierto.              GTKWave R1 -> X"00000002"
-LW R1, 8(R0)    -- Acierto.              GTKWave R1 -> X"00000003"
-LW R1, 12(R0)    -- Acierto.              GTKWave R1 -> X"00000004"
+LW R1, 0(R0)    -- Read Miss Clean (Carga Vía 0).   R1 -> X"00000001"
+LW R1, 4(R0)    -- Read Hit.                        R1 -> X"00000002"
+LW R1, 8(R0)    -- Read Hit.                        R1 -> X"00000003"
+LW R1, 12(R0)   -- Read Hit.                        R1 -> X"00000004"
 
 -- Set 1
-LW R1, 16(R0)   -- Fallo (Carga Vía 0).  GTKWave R1 -> X"00000005"
-LW R1, 20(R0)   -- Acierto.              GTKWave R1 -> X"00000006"
+LW R1, 16(R0)   -- Read Miss Clean (Carga Vía 0).   R1 -> X"00000005"
+LW R1, 20(R0)   -- Read Hit.                        R1 -> X"00000006"
 
 -- Set 2
-LW R1, 32(R0)   -- Fallo (Carga Vía 0).  GTKWave R1 -> X"00000009"
-LW R1, 36(R0)   -- Acierto.              GTKWave R1 -> X"0000000A"
+LW R1, 32(R0)   -- Read Miss Clean (Carga Vía 0).   R1 -> X"00000009"
+LW R1, 36(R0)   -- Read Hit.                        R1 -> X"0000000A"
 
 -- Set 3
-LW R1, 48(R0)   -- Fallo (Carga Vía 0).  GTKWave R1 -> X"0000000D"
-LW R1, 52(R0)   -- Acierto.              GTKWave R1 -> X"0000000E"
+LW R1, 48(R0)   -- Read Miss Clean (Carga Vía 0).   R1 -> X"0000000D"
+LW R1, 52(R0)   -- Read Hit.                        R1 -> X"0000000E"
 
 
--- ==================================================================
--- FASE 2: Llenado de la VÍA 1 en TODOS LOS CONJUNTOS (Sets 0 al 3)
--- ==================================================================
+-- Vía 1
 -- Set 0
-LW R1, 64(R0)   -- Fallo (Carga Vía 1).  GTKWave R1 -> X"00000011"
-LW R1, 68(R0)   -- Acierto.              GTKWave R1 -> X"00000012"
+LW R1, 64(R0)   -- Read Miss Clean (Carga Vía 1).   R1 -> X"00000011"
+LW R1, 68(R0)   -- Read Hit.                        R1 -> X"00000012"
 
 -- Set 1
-LW R1, 80(R0)   -- Fallo (Carga Vía 1).  GTKWave R1 -> X"00000015"
-LW R1, 84(R0)   -- Acierto.              GTKWave R1 -> X"00000016"
-LW R1, 88(R0)   -- Acierto.              GTKWave R1 -> X"00000017"
-LW R1, 92(R0)   -- Acierto.              GTKWave R1 -> X"00000018"
+LW R1, 80(R0)   -- Read Miss Clean (Carga Vía 1).   R1 -> X"00000015"
+LW R1, 84(R0)   -- Read Hit.                        R1 -> X"00000016"
+LW R1, 88(R0)   -- Read Hit.                        R1 -> X"00000017"
+LW R1, 92(R0)   -- Read Hit.                        R1 -> X"00000018"
 
 -- Set 2
-LW R1, 96(R0)   -- Fallo (Carga Vía 1).  GTKWave R1 -> X"00000019"
-LW R1, 100(R0)  -- Acierto.              GTKWave R1 -> X"0000001A"
+LW R1, 96(R0)   -- Read Miss Clean (Carga Vía 1).   R1 -> X"00000019"
+LW R1, 100(R0)  -- Read Hit.                        R1 -> X"0000001A"
 
 -- Set 3
-LW R1, 112(R0)  -- Fallo (Carga Vía 1).  GTKWave R1 -> X"0000001D"
-LW R1, 116(R0)  -- Acierto.              GTKWave R1 -> X"0000001E"
+LW R1, 112(R0)  -- Read Hit Miss Clean (Carga Vía 1).   R1 -> X"0000001D"
+LW R1, 116(R0)  -- Read Hit.                            R1 -> X"0000001E"
 
 
--- ==================================================================
--- FASE 3: Reemplazo FIFO de la VÍA 0 en TODOS LOS CONJUNTOS
--- ==================================================================
--- La caché está llena. Entran nuevos bloques limpios que expulsan
--- a los más antiguos (los cargados en la Fase 1 en la Vía 0).
-
+-- Reemplazo de la vía 0 en todos los conjuntos
 -- Set 0
-LW R1, 128(R0)  -- Fallo (Reemplazo).    GTKWave R1 -> X"00000021"
-LW R1, 132(R0)  -- Acierto.              GTKWave R1 -> X"00000022"
+LW R1, 128(R0)  -- Read Hit Miss Clean (Reemplazo). R1 -> X"00000021"
+LW R1, 132(R0)  -- Read Hit Hit.                    R1 -> X"00000022"
 
 -- Set 1
-LW R1, 144(R0)  -- Fallo (Reemplazo).    GTKWave R1 -> X"00000025"
-LW R1, 148(R0)  -- Acierto.              GTKWave R1 -> X"00000026"
+LW R1, 144(R0)  -- Read Miss Clean (Reemplazo). R1 -> X"00000025"
+LW R1, 148(R0)  -- Read Hit.                    R1 -> X"00000026"
 
 -- Set 2
-LW R1, 160(R0)  -- Fallo (Reemplazo).    GTKWave R1 -> X"00000029"
-LW R1, 164(R0)  -- Acierto.              GTKWave R1 -> X"0000002A"
-LW R1, 168(R0)  -- Acierto.              GTKWave R1 -> X"0000002B"
-LW R1, 172(R0)  -- Acierto.              GTKWave R1 -> X"0000002C"
+LW R1, 160(R0)  -- Read Miss Clean (Reemplazo).    R1 -> X"00000029"
+LW R1, 164(R0)  -- Read Hit.                    R1 -> X"0000002A"
+LW R1, 168(R0)  -- Read Hit.                    R1 -> X"0000002B"
+LW R1, 172(R0)  -- Read Hit.                    R1 -> X"0000002C"
 
 -- Set 3
-LW R1, 176(R0)  -- Fallo (Reemplazo).    GTKWave R1 -> X"0000002D"
-LW R1, 180(R0)  -- Acierto.              GTKWave R1 -> X"0000002E"
+LW R1, 176(R0)  -- Read Miss Clean (Reemplazo). R1 -> X"0000002D"
+LW R1, 180(R0)  -- Read Hit.                    R1 -> X"0000002E"
