9d65e5333b26dc5432727adcf0a039e9f0bbf39a
SnsRizos <926029@unizar.es>
2026-05-09 14:48:46 +0200

    Arreglos (igual queda cambiar algunas cosas)

--- Pruebas/internal.asm
+++ Pruebas/internal.asm
@@ -1,4 +1,4 @@
-LW R3, 0(R0)    -- R3 = X"01000000" (Read Miss Clean)
-LW R2, 4(R0)    -- R2 = X"00000002" (Read Hit)
+LW R3, 4(R0)    -- R3 = X"01000000" (Read Miss Clean)
+LW R2, 8(R0)    -- R2 = X"00000002" (Read Hit)
 
 SW R2, 0(R3)    -- Intento de escritura en registro interno. Activa Mem_ERROR
--- Pruebas/outOfRange.asm
+++ Pruebas/outOfRange.asm
@@ -1,2 +1,2 @@
-LW R1, 0(R0)    -- R1 = X"00000400" (Dirección inválida, fuera de rango)
+LW R1, 4(R0)    -- R1 = X"00000400" (Dirección inválida, fuera de rango)
 LW R3, 0(R1)    -- Intento de lectura. ¡Se activa Mem_ERROR y ocurre un Data Abort!
