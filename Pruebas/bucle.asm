b379f359630dac2c2fe3390e04a34e66427a80c6
SnsRizos <926029@unizar.es>
2026-05-10 11:58:54 +0200

    cod

--- Pruebas/bucle.asm
+++ Pruebas/bucle.asm
@@ -1,5 +1,5 @@
-LW R3, 0(R0)   -- Read Hit. R1 ->  X"00000001"
-LW R1, 4(R0)    -- Read Miss Clean (Carga Vía 0).   R3 -> X"00000000"
+LW R3, 0(R0)   -- Read Miss Clean. R1 ->  X"00000001"
+LW R1, 4(R0)    -- Read Hit (Carga Vía 0).   R3 -> X"00000000"
 LW R4, 8(R0)    -- Read Hit. R4 ->  X"00000016"
 LW R2, 12(R0)   -- Read Hit. R2 ->  X"10000000"
 
@@ -25,7 +25,7 @@ LW R1, 80(R0)   -- Read Miss Clean (Carga Vía 1).   R1 -> X"00000015"
 -- Set 2
 LW R1, 96(R0)   -- Read Miss Clean (Carga Vía 1).   R1 -> X"00000019"
 -- Set 3
-LW R1, 112(R0)  -- Read Hit Miss Clean (Carga Vía 1).   R1 -> X"0000001D"
+LW R1, 112(R0)  -- Read Miss Clean (Carga Vía 1).   R1 -> X"0000001D"
 
 LW R1, 4(R2)   -- Read Scratch (@X"10000004") R1 -> Contador
 
